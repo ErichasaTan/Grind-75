e76241fd60e55416e40c7b9b32b5a6414898afd2
Eric Tan <43790496+ErichasaTan@users.noreply.github.com>
2024-07-25 23:30:32 -0700

    Update TwoSum.py

--- Week-1/TwoSum.py
+++ Week-1/TwoSum.py
@@ -1,5 +1,7 @@
 # Author Eric & Manpreet
 # Date: 7/25/24
+# Time / Space Complexity: O(n2) because there are two for loops
+# Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity? - We will revisit this problem in the future
 class Solution(object):
     def twoSum(self, nums, target):
         """
